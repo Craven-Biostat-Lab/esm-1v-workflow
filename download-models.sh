bdd4a2d4d0c7e05372ecc1c8deda188e87bc1013
Yuriy Sverchkov <sverchkov@wisc.edu>
2023-08-28 16:33:04 -0500

    Changed model location for docker

--- download-models.sh
+++ download-models.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-mkdir -p resources/
-cd resources
+mkdir -p container/esm_models
+cd container/esm_models
 
 for i in $(seq 1 5);
 do
     wget https://dl.fbaipublicfiles.com/fair-esm/models/esm1v_t33_650M_UR90S_${i}.pt
 done
 
-cd ..
+cd ../..
