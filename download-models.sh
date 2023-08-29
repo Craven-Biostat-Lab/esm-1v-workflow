84714831f5428e9c943794208fcbdfe88631611f
Yuriy Sverchkov <sverchkov@wisc.edu>
2023-08-29 15:08:42 -0500

    Moved ESM models to container dir

--- download-models.sh
+++ download-models.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
-mkdir -p resources/
-cd resources
+MODEL_LOCATION='container/esm_models'
+OWD=$(pwd)
+
+mkdir -p $MODEL_LOCATION
+cd $MODEL_LOCATION
 
 for i in $(seq 1 5);
 do
     wget https://dl.fbaipublicfiles.com/fair-esm/models/esm1v_t33_650M_UR90S_${i}.pt
 done
 
-cd ..
+cd $OWD
