b403638476a88cf1b7d656e3354e2fa9ba2eb172
Yuriy Sverchkov <yuriy@biostat.wisc.edu>
2023-09-13 14:58:01 -0500

    Local test with Ubiquitin

---
+++ local-test/UBP14-test.sh
@@ -0,0 +1,5 @@
+python ../container/predict_substitutions.py \
+    --model-location /ua/ml-group/igvf/repos/esm-1v-workflow/resources/esm1v_t33_650M_UR90S_5.pt \
+    --sequences UBP14.fasta \
+    --results UBP14-local-result.csv \
+    --batch-size 5
