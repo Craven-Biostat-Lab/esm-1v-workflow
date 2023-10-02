744c4e0fd6858f0dfcf0ddc20b025e2624559772
Yuriy Sverchkov <yuriy@biostat.wisc.edu>
2023-10-02 16:45:37 -0500

    Modified result-processing This way output bundles are read directly

---
+++ process-results.sh
@@ -0,0 +1,6 @@
+# Process results shell script
+# Intended to be called by condor submit file
+
+/ua/yuriy/mambaforge/envs/esm-1v-processing/bin/python process-results.py \
+    --source chtc/proteins/missing-mane/missing-mane/bundle_${1}_m*.csv \
+    --output-dir results/processed-mane
