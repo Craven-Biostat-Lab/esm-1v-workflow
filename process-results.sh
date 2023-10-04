039ad42e75548e36757f29303830bfa63c9effec
Yuriy Sverchkov <yuriy@biostat.wisc.edu>
2023-10-04 14:13:48 -0500

    Updated submit files for (re)processing results

--- process-results.sh
+++ process-results.sh
@@ -3,4 +3,5 @@
 
 /ua/yuriy/mambaforge/envs/esm-1v-processing/bin/python process-results.py \
     --source chtc/proteins/missing-mane/missing-mane/bundle_${1}_m*.csv \
-    --output-dir results/processed-mane
+    --output-dir results/processed-mane \
+    --overwrite
