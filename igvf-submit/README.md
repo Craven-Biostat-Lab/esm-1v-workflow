54297308abcd530c1c6ec2e41322f174d99f7f72
Yuriy Sverchkov <yuriy@biostat.wisc.edu>
2024-12-13 17:04:46 -0600

    Updated test submission files for igvf

---
+++ igvf-submit/README.md
@@ -0,0 +1,24 @@
+# ESM-1v predictions submitted to the IGVF portal
+
+Here we describe our submission of the ESM-1v predictions.
+
+The main product of the submission is predictions for all single amino acid substitutions across all MANE transcripts.
+The predictions are organized in prediction sets grouped by gene.
+
+## Portal objects:
+
+- **prediction file**: TSV file for all predictions on a given protein (specific ENSP ID). Links to:
+    - **prediction file set**
+    - **format specification**: Document describing format. We use our documentation object for this.
+- **prediction file set**: Group of prediction files for a given gene (specific ENSG ID). Links to:
+    - **documents**: We link to our documentation object.
+- **documentation**: PDF generated from `ESM-1v Predictions Documentation.md`
+- **model file**: A specific ESM-1v weight file. Link to **moel set**
+- **model set**: Represents ESM-1v. Links to:
+    - **software version**
+    - **external input data** (do we want this?)
+- **software version**: version of this repository.
+    - Links to **software**.
+    - Links to this repository.
+    - References commit ID.
+- **software**: this repository.
--- igvf-submit/submit-results.sh
+++ igvf-submit/submit-results.sh
@@ -3,23 +3,60 @@
 # Must have the environment variables IGVF_API_KEY and IGVF_SECRET_KEY set.
 
 # Submit documentation
-iu_register.py -d \
-    -m sandbox \
-    -p document \
-    -i esm-1v-documentation.json
+if false; then
+    iu_register.py \
+        -m sandbox \
+        -p document \
+        -i esm-1v-documentation.json
+fi
 
-# Patch the already submitted prediction set
-iu_register.py -d \
-    -m sandbox \
-    -p prediction_set \
-    -i esm-1v-predictions.json \
-    --patch
+# Submit software
+if false; then
+    iu_register.py \
+        -m sandbox \
+        -p software \
+        -i esm-1v-software.json
+fi
 
-# Submit files
-iu_register.py \
-    -m sandbox \
-    -p tabular_file \
-    -i esm-1v-files-metadata.txt
+# Submit software version
+if false; then
+    iu_register.py \
+        -m sandbox \
+        -p software_version \
+        -i esm-1v-software-version.json
+fi
+
+# Submit model set
+if false; then
+    iu_register.py \
+        -m sandbox \
+        -p model_set \
+        -i esm-1v-model-set.json
+fi
+
+# Submit model files
+if false; then
+    iu_register.py \
+        -m sandbox \
+        -p model_file \
+        -i esm-1v-model-files-test.json
+fi
+
+# Submit prediction sets
+if false; then
+    iu_register.py \
+        -m sandbox \
+        -p prediction_set \
+        -i esm-1v-fileset-metadata.txt
+fi
+
+# Submit prediction files
+if true; then
+    iu_register.py \
+        -m sandbox \
+        -p tabular_file \
+        -i esm-1v-files-metadata.txt
+fi
 
 # Explanation of arguments;
 # -m (mode) is either sandbox or prod (for production)
